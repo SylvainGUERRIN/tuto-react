c1b0fb3d301150c5631b77a121a502ef0f432752
SylvainG <sguerrin549@gmail.com>
2021-11-21 14:20:28 +0100

    react component with classes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         value: null,
+    //     }
+    // }
+
     render() {
         return (
-            <button className="square" onClick={() => alert('clic') }>
+            <button
+                className="square"
+                onClick={() => this.props.onClick() }
+            >
                 {/* TODO */}
                 {this.props.value}
             </button>
@@ -14,12 +24,30 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Prochain joueur : X';
 
         return (
             <div>
@@ -42,6 +70,7 @@ class Board extends React.Component {
             </div>
         );
     }
+
 }
 
 class Game extends React.Component {
